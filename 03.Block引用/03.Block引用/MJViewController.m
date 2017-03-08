459de07a87b5facabccb7227f30636ccf1c37c5f
mrhyh <mrhyh@qq.com>
2017-03-08 21:43:24 +0800

    多线程Demo集合

---
+++ 03.Block引用/03.Block引用/MJViewController.m
@@ -0,0 +1,34 @@
+//
+//  MJViewController.m
+//  03.Block引用
+//
+//  Created by apple on 14-4-23.
+//  Copyright (c) 2014年 itcast. All rights reserved.
+//
+
+#import "MJViewController.h"
+#import "DemoObj.h"
+
+@interface MJViewController ()
+
+@end
+
+@implementation MJViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    DemoObj *obj = [[DemoObj alloc] init];
+    [obj demoBlockOp];
+    
+    [self executeAction];
+}
+
+- (void ) executeAction {
+    
+    self.viewDidScroll(@"111");
+    
+}
+
+@end
---
+++ Timer/README.md
@@ -0,0 +1,11 @@
+# JX_GCDTimerManager
+
+在[JX_GCDTimer ](https://github.com/Joeyqiushi/JX_GCDTimer)基础上添加了几个其他定时器的Demo.
+
+NSTimer like tool implemented using GCD.
+
+使用时只需将“JX_GCDTimerManager.h”和“JX_GCDTimerManager.m”加入工程。
+
+在需要使用的文件里引入“JX_GCDTimerManager.h”，“JX_GCDTimerManager.h”里提供了三个接口。
+
+当然你也可以选择按自己的需求修改代码、增删接口：）
